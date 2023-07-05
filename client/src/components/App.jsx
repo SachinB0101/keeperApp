9e738fd9424ff7dec4983c4eca64136ae17560cf
Sachin <sachinbhatt@wifi-wpa-cw-140-193-210-124.wifi.umanitoba.ca>
2023-07-05 13:08:20 -0500

    add login page route

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,43 +1,16 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+
+import Home from "./Home";
+import Login from "./Login";
+
 
 function App() {
-  const [notes, setNotes] = useState([]);
-
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
-  }
-
-  function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
-  }
-
   return (
-    <div>
-      <Header />
-      <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => {
-        return (
-          <Note
-            key={index}
-            id={index}
-            title={noteItem.title}
-            content={noteItem.content}
-            onDelete={deleteNote}
-          />
-        );
-      })}
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+    </Routes>
   );
 }
 
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
